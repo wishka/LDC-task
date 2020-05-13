df09e4f5c022f2f788f3c96b8cd75f02cdf2cd63
wishka <roucke@mail.ru>
2020-05-13 14:23:40 +0300

    complite users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root             'static_page#welcome'
-
-  get 'help' =>    'static_page#help'
-  get 'about' =>   'static_page#about'
-  get 'contact' => 'static_page#contact'
-  get 'signup'  => 'users#new'
+  get 'help'       => 'static_page#help'
+  get 'about'      => 'static_page#about'
+  get 'contact'    => 'static_page#contact'
+  get 'signup'     => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_231122) do
+ActiveRecord::Schema.define(version: 2020_05_13_104425) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_05_12_231122) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
+    t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
