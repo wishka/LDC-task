1359e064d77a7ea94c7d69bc8437ab5da85a7016
wishka <roucke@mail.ru>
2020-05-26 00:29:09 +0300

    correcting button name

--- time_helper.rb
+++ time_helper.rb
@@ -1,6 +1,6 @@
 time = Time.new
 
-arr_time = {'1' => 'Monday', '2' => 'Thueday', '3' => 'Wednesday', '4' => 'Thursday', '5' => 'Friday',
+arr_time = {'1' => 'Monday', '2' => 'Tuesday', '3' => 'Wednesday', '4' => 'Thursday', '5' => 'Friday',
          '6' => 'Saturday', '7' => 'Sunday'}
 week_day = time.wday.to_s
 
